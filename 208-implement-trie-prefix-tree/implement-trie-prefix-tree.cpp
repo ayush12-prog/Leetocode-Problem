a5968ce513bf84aa87c5b471778dd71793d06121
ayush12-prog <ayushkumar991890@gmail.com>
2025-05-16 22:06:16 +0530

    Time: 15 ms (88.61%) | Memory: 50.6 MB (32.39%) - LeetSync

---
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
@@ -0,0 +1,34 @@
+// OJ: https://leetcode.com/problems/implement-trie-prefix-tree/
+// Time: O(W) for insert/search/startsWith
+// Space: O(1) extra space for all
+struct TrieNode {
+    TrieNode *next[26] = {};
+    bool word = false;
+};
+class Trie {
+    TrieNode root;
+    TrieNode *find(string &word) {
+        auto node = &root;
+        for (char c : word) {
+            if (!node->next[c - 'a']) return NULL;
+            node = node->next[c - 'a'];
+        }
+        return node;
+    }
+public:
+    void insert(string word) {
+        auto node = &root;
+        for (char c : word) {
+            if (!node->next[c - 'a']) node->next[c - 'a'] = new TrieNode();
+            node = node->next[c - 'a'];
+        }
+        node->word = true;
+    }
+    bool search(string word) {
+        auto node = find(word);
+        return node && node->word;
+    }
+    bool startsWith(string prefix) {
+        return find(prefix);
+    }
+};
