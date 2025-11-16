dac291f69a3ce6ae92906b2495c404547e84b064
ayush12-prog <ayushkumar991890@gmail.com>
2025-11-16 17:56:31 +0530

    Time: 4 ms (19.79%) | Memory: 11.5 MB (72.24%) - LeetSync

---
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.cpp
@@ -0,0 +1,19 @@
+class Solution {
+ public:
+  int numSub(string s) {
+    constexpr int kMod = 1'000'000'007;
+
+    int ans = 0;
+    int l = -1;
+
+    for (int i = 0; i < s.length(); ++i) {
+      if (s[i] == '0')
+        l = i;  
+      ans = (ans + i - l) % kMod;
+    }
+
+    return ans;
+  }
+};
+
+
