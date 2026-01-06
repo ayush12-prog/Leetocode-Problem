d9b632c64a022da0cf5832b444c22ea46994488f
Ayush Kumar <ayushkumar991890@gmail.com>
2026-01-06 22:02:18 +0530

    Time: 11 ms (12.88%) | Memory: 48.7 MB (90.75%) - LeetSync

--- 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.java
+++ 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.java
@@ -1,43 +1,19 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public int maxLevelSum(TreeNode root) {
-        Deque<TreeNode> q = new ArrayDeque<>();
-        q.offer(root);
-        int mx = Integer.MIN_VALUE;
-        int i = 0;
-        int ans = 0;
-        while (!q.isEmpty()) {
-            ++i;
-            int s = 0;
-            for (int n = q.size(); n > 0; --n) {
-                TreeNode node = q.pollFirst();
-                s += node.val;
-                if (node.left != null) {
-                    q.offer(node.left);
-                }
-                if (node.right != null) {
-                    q.offer(node.right);
-                }
-            }
-            if (mx < s) {
-                mx = s;
-                ans = i;
-            }
-        }
-        return ans;
-    }
+  public int maxLevelSum(TreeNode root) {
+    // levelSums[i] := the sum of level (i + 1) (1-indexed)
+    List<Integer> levelSums = new ArrayList<>();
+    dfs(root, 0, levelSums);
+    return 1 + IntStream.range(0, levelSums.size())
+                   .reduce(0, (a, b) -> levelSums.get(a) < levelSums.get(b) ? b : a);
+  }
+
+  private void dfs(TreeNode root, int level, List<Integer> levelSums) {
+    if (root == null)
+      return;
+    if (levelSums.size() == level)
+      levelSums.add(0);
+    levelSums.set(level, levelSums.get(level) + root.val);
+    dfs(root.left, level + 1, levelSums);
+    dfs(root.right, level + 1, levelSums);
+  }
 }
