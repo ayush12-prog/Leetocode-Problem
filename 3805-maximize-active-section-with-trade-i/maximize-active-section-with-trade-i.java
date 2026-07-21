5e5141207996e8ba4de2dcfb2477e217db2f265c
Ayush Kumar <ayushkumar991890@gmail.com>
2026-07-21 19:15:39 +0530

    Time: 153 ms (19.72%) | Memory: 48.1 MB (54.93%) - LeetSync

--- 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.java
+++ 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.java
@@ -1,46 +1,21 @@
 class Solution {
-    public int maxActiveSectionsAfterTrade(String s) {
-        String ss = "1" + s + "1";
-        int count = 0;
-        int maxbtz = 0;
+  public int maxActiveSectionsAfterTrade(String s) {
+    List<Integer> zeroGroupLengths = new ArrayList<>();
+    int maxZeroMerge = 0;
 
-        for (int i = 0; i < ss.length(); i++) {
-            if (ss.charAt(i) == '1') {
-                count++;
-            }
-        }
-        maxbtz = count;
-        
-        int max = maxbtz-2;
-        int j = 1;
-        
-        while (j < ss.length() && ss.charAt(j) != '0') {
-            j++;
-        }
-        
-        int start = j;
-        boolean one = false;
-        int nxtstart = j;
-        
-        while (j < ss.length()) {
-            if (ss.charAt(j) == '0') {
-                j++;
-                continue;
-            } else {
-                if (one) {
-                    max = Math.max(max, count + (j - start)-2);
-                    start = nxtstart;
-                    count = maxbtz;
-                }
-                one = true;
-                
-                while (j < ss.length() && ss.charAt(j) != '0') {
-                    j++;
-                    count--;
-                }
-                nxtstart = j;
-            }
-        }
-        return max;
+    for (int i = 0; i < s.length(); ++i) {
+      if (s.charAt(i) == '0') {
+        if (i > 0 && s.charAt(i - 1) == '0')
+          zeroGroupLengths.set(zeroGroupLengths.size() - 1,
+                               zeroGroupLengths.get(zeroGroupLengths.size() - 1) + 1);
+        else
+          zeroGroupLengths.add(1);
+      }
     }
+
+    for (int i = 0; i < zeroGroupLengths.size() - 1; ++i)
+      maxZeroMerge = Math.max(maxZeroMerge, zeroGroupLengths.get(i) + zeroGroupLengths.get(i + 1));
+
+    return (int) s.chars().filter(c -> c == '1').count() + maxZeroMerge;
+  }
 }
