0cd5f8e45bb9fda6c27347e88911cd71ed5de7b4
Ayush Kumar <ayushkumar991890@gmail.com>
2026-08-31 14:05:25 +0530

    Time: 10 ms (20.08%) | Memory: 43.1 MB (55.15%) - LeetSync

--- 38-count-and-say/count-and-say.java
+++ 38-count-and-say/count-and-say.java
@@ -1,19 +1,24 @@
 class Solution {
     public String countAndSay(int n) {
         String s = "1";
-        while (--n > 0) {
-            StringBuilder t = new StringBuilder();
-            for (int i = 0; i < s.length();) {
-                int j = i;
-                while (j < s.length() && s.charAt(j) == s.charAt(i)) {
-                    ++j;
+        
+        for (int step = 2; step <= n; step++) {
+            StringBuilder sb = new StringBuilder();
+            int count = 1;
+            
+            for (int i = 0; i < s.length(); i++) {
+                // If next character is the same, increment the count
+                if (i + 1 < s.length() && s.charAt(i) == s.charAt(i + 1)) {
+                    count++;
+                } else {
+                    // Group ended; append count followed by the digit
+                    sb.append(count).append(s.charAt(i));
+                    count = 1; // reset for next group
                 }
-                t.append((j - i) + "");
-                t.append(s.charAt(i));
-                i = j;
             }
-            s = t.toString();
+            s = sb.toString();
         }
+        
         return s;
     }
 }
